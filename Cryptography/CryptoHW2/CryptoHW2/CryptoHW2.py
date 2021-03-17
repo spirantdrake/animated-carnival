c5d669f17c3e67bdf33b94943128ea6adce5a165
ChipTheKing <chiplyons1@att.net>
2021-03-17 14:08:47 -0400

    Finished Pre(P) function
    added math import and formatted pre(p) function so that it works as it should. issue#2-1 is completed.

--- Cryptography/CryptoHW2/CryptoHW2/CryptoHW2.py
+++ Cryptography/CryptoHW2/CryptoHW2/CryptoHW2.py
@@ -1,12 +1,12 @@
 from itertools import chain
-
+import math
 
 
 def key(k):
     # replaces the j to i so there is only one of the two, creates a temp list and creates an english alphabet string
     temp_key = k.replace("j", "i")
     temp = []
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    alphabet = "abcdefghiklmnopqrstuvwxyz"
 
     #splits the list of characters
     for x in chain(temp_key, alphabet):
@@ -14,30 +14,42 @@ def key(k):
 
     #creates a matrix using the list slices
     KM = [temp[y:y+5] for y in range(0, 25, 5)]
-    #print(KM)
-    return(KM)
+    print(KM)
+    #return(KM)
 
 
 def Pre(P):
-    #P.split()
+    #variables for manipulating data and the strings
     PL = []
     y = 0 
     z = 1
-    for x in range(len(P)):
-        if(y == 0):
-            PL.append(P[y])
-        elif(P[y] != P[y-1]):
-            PL.append(P[y])
-        else:
-            PL.append("x")
-            x -= 1
-        y += 1
+    word_length = int(math.ceil(len(P)/2))
+    repitition = False
+
+    # loops through the given plaintext and places the letters into a list 2 at a time
+    for x in range(word_length):
+        if(z < len(P)):
+            if(P[y] == P[z]): #checks to see if two side by side letters are the same and inserts an x if they are
+                PL.append(P[y]+"x")
+                y -= 1
+                z -= 1
+                repitition = True
+            else: #adds both characters into the list
+                PL.append(P[y]+P[z])
+        else: #if the list is an odd number it adds an x as the last character to make it even
+            PL.append(P[y]+"x")
+        y += 2
+        z += 2
+    #edge case rule for catching even numbered plaintext that contain repititive letters
+    if (len(P)%2 == 0 and repitition == True):
+        PL.append(P[y] + "x")
     print(PL)
+    #return(PL)
 
 
 def __main__():
-    #k = input("Enter the key for your Playfair cipher: \n")
-    #key(k.lower())
+    k = input("Enter the key for your Playfair cipher: \n")
+    key(k.lower())
     P = input("Enter your plaingtext you wish to encode: \n")
     Pre(P.lower())
 
